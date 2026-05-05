2d460cb0c67d7f3c6fd292ca2cd1da16e26ee263
Kaleb014 <kaleb.d.douglas@gmail.com>
2026-05-04 21:39:20 -0500

    various changes
    various changes

--- obj/Debug/net6.0/Practice.AssemblyInfo.cs
+++ obj/Debug/net6.0/Practice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c30dca2b1907ecb8404254ccc8916c686b2fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a12dcefde685f09a93f8088bee62eaabf3eb80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
